1e0ce000b13dae9bd5b63869992b132bbad2cb55
Andrei Radu <raduandrei697@gmail.com>
2023-09-29 23:54:17 +0300

    add request block duration

--- rate-limiter.ts
+++ rate-limiter.ts
@@ -1,10 +1,19 @@
 import RateLimitError from "./rate-limit-error";
 
+interface RateLimiterIP {
+  ip: string;
+  blocked: boolean;
+  last_request_timestamp: number;
+  blocked_until_timestamp: number;
+  requestCount: number;
+}
+
 class RateLimiter {
   private readonly maxBucketSize = 120;
   private readonly bucketLimiter = 3;
-  private bucketMap = new Map<string, number>();
+  private bucketMap = new Map<string, RateLimiterIP>();
   private static instance: RateLimiter | null = null;
+  private readonly not_allowed_calls_minutes = 1;
 
   constructor() {
     if (RateLimiter.instance) {
@@ -27,14 +36,55 @@ class RateLimiter {
       );
     }
 
-    if (!this.bucketMap.get(ip)) {
-      this.bucketMap.set(ip, 1);
+    const existingIP = this.bucketMap.get(ip);
+    const now = Date.now();
+    if (!existingIP) {
+      this.bucketMap.set(ip, {
+        ip: ip,
+        blocked: false,
+        blocked_until_timestamp: 0,
+        last_request_timestamp: now,
+        requestCount: 1,
+      });
     } else {
-      let callNo = this.bucketMap.get(ip) || 0;
-      if (callNo < this.bucketLimiter) {
-        this.bucketMap.set(ip, ++callNo);
+      const {
+        blocked,
+        blocked_until_timestamp,
+        last_request_timestamp,
+        requestCount,
+      } = existingIP;
+
+      if (requestCount >= this.bucketLimiter) {
+        if (!blocked) {
+          this.bucketMap.set(ip, {
+            ...existingIP,
+            blocked: true,
+            last_request_timestamp: now,
+            blocked_until_timestamp:
+              now + this.not_allowed_calls_minutes * 60000,
+          });
+
+          throw new RateLimitError(`Limit exceeded for IP ${ip}`);
+        } else {
+          if (now > blocked_until_timestamp && now > last_request_timestamp) {
+            console.log("Limit exceeded, but time expired => reset");
+            this.bucketMap.set(ip, {
+              ...existingIP,
+              blocked: false,
+              last_request_timestamp: now,
+              blocked_until_timestamp: 0,
+              requestCount: 1,
+            });
+          }
+        }
       } else {
-        throw new RateLimitError(`Limit exceeded for ip: ${ip}`);
+        this.bucketMap.set(ip, {
+          ...existingIP,
+          blocked: false,
+          last_request_timestamp: now,
+          blocked_until_timestamp: 0,
+          requestCount: requestCount + 1,
+        });
       }
     }
   }
@@ -45,13 +95,20 @@ class RateLimiter {
            
    */
 }
-
 const ip1 = new RateLimiter();
-ip1.triggerCall("192.168.0.1");
-ip1.triggerCall("192.168.0.1");
 const ip2 = new RateLimiter();
-ip2.triggerCall("192.168.0.1");
-ip2.triggerCall("192.168.0.2");
-ip2.triggerCall("192.168.0.2");
-ip2.triggerCall("192.168.0.2");
-ip2.triggerCall("192.168.0.2"); // Limit Exceeded
+try {
+  ip1.triggerCall("192.168.0.1");
+  ip1.triggerCall("192.168.0.1");
+
+  ip2.triggerCall("192.168.0.1");
+  ip2.triggerCall("192.168.0.2");
+  ip2.triggerCall("192.168.0.2");
+  ip2.triggerCall("192.168.0.2");
+  ip2.triggerCall("192.168.0.2"); // Limit Exceeded
+  console.log("call again after exceeding limit");
+  ip2.triggerCall("192.168.0.2");
+} catch (error) {
+  console.log("calling again from catch");
+  ip2.triggerCall("192.168.0.2");
+}
